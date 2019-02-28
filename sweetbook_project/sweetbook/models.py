9b6909d22297d7430df62fcf279860c6e40d3772
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2019-02-28 18:53:48 +0000

    updating models.py

--- sweetbook_project/sweetbook/models.py
+++ sweetbook_project/sweetbook/models.py
@@ -30,6 +30,7 @@ class Recipe(models.Model):
     rating = models.DecimalField(decimal_places=2, max_digits=3)
     cooktime = models.IntegerField()
     difficulty = models.CharField(max_length=10)
+    last_modified = models.DateTimeField()
 
     def __str__(self):
         return self.AuthenticationMiddleware
