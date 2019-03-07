7d10854d43c19b6f1ef2943898d855b02761d408
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2019-03-07 22:30:03 +0000

    added cookies

--- sweetbook_project/sweetbook/views.py
+++ sweetbook_project/sweetbook/views.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'sweetbook_project.settings')
 
@@ -10,6 +12,7 @@ from sweetbook.models import User, Event, Recipe, SavedRecipe, Comment
 
 
 def home(request):
+    request.session.set_test_cookie()
     top_rated_recipes = Recipe.objects.order_by('rating')[::-1][:10]
     most_commented_recipes= []
     context_dict = {}
@@ -23,10 +26,18 @@ def home(request):
     context_dict ["top rated recipes"] = top_rated_recipes
     context_dixt ["recipe of the day"] = recipe_of_the_day
     context_dict ["latest events"] = latest_events
-    return render(request, 'sweetbook/home.html', context_dict )
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'sweetbook/home.html', context=context_dict)
+    return response
 
 
 def recipes (request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
 	last_recipes = Recipe.objects.order_by('last_modified')[:20]
 	context_dict["recipes"] = last_recipes
 	return render(request, 'sweetbook/recipes.html', context_dict)
@@ -83,3 +94,21 @@ def chosen_event(request, event_slug):
     except Category.DoesNotExist:
         context_dict['event'] = None
     return render (request, 'sweetbook/chosen_event.html', context_dict)
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
