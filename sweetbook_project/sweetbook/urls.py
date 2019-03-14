eebbb9c27729965d4f63c328a36738fd156a6880
Elena Soare <2309307s@student.gla.ac.uk>
2019-03-14 15:23:55 +0000

    Added some views

--- sweetbook_project/sweetbook/urls.py
+++ sweetbook_project/sweetbook/urls.py
@@ -15,7 +15,10 @@ urlpatterns = [
     url(r'^recipes/(?P<recipe_slug>[\w\-]+)/add-comment/$',
     	  views.add_comment, name = 'add_comment'),
     url (r'^register/$', views.register, name='register'),
-
+    url (r'^myaccount/$', views.myaccount, name='myaccount'),
+    url (r'^myaccount/mybakebook/$', views.mybakebook, name='mybakebook'),
+    url (r'^myaccount/mycalendar/$', views.mycalendar, name='mycalendar'),
+    url (r'^myaccount/myrecipes/$', views.myrecipes, name='myrecipes'),
     url (r'^login/$', views.user_login, name="login"),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^like_recipe/$', views.like_recipe, name='like_recipe'),
